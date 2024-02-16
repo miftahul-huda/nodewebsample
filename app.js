867cde427ef905221bf14e7930c5d54fd52bb347
M. Huda <mhuda@Ms-MacBook-Pro.local>
2024-02-16 15:33:39 +0700

    remove session

--- app.js
+++ app.js
@@ -35,13 +35,6 @@ app.use(express.json({type: '*/*'}));
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(session({
-  store: new DatastoreStore({
-    dataset: new Datastore(),
-    kind: 'express-sessions',
-  }),
-  secret: 'levenshtein',saveUninitialized: true,resave: false}));
-
 //Dynamic routing based on configuration
 const fs = require('fs');
 let rawdata = fs.readFileSync('route-config.json');
@@ -82,20 +75,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-
 
 app.listen(port)
 
